db5e60d1f83f0e10c90e1bff459941968698a44d
Fede <federicoqueirel@hotmail.com>
2023-10-15 23:52:08 -0300

    fixes

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -52,41 +52,18 @@ const User = () => {
           <p><button className="primary ghost">Delete</button></p>
         </div>
         <div className="skills">
-          <h6>Skills</h6>
+          <h6>Accessibilities</h6>
           <ul>
-            <li>UI / UX</li>
-            <li>Front End Development</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>Node</li>
+            <li>Etc</li>
+            <li>Etc</li>
+            <li>Etc</li>
+            <li>Etc</li>
+            <li>Etc</li>
+            <li>Etc</li>
+            <li>Etc</li>
           </ul>
         </div>
       </div>
-
-      <footer>
-        <p>
-          Created with <i className="fa fa-heart"></i> by
-          <a target="_blank" href="https://florin-pop.com">
-            Florin Pop
-          </a>
-          - Read how I created this
-          <a
-            target="_blank"
-            href="https://florin-pop.com/blog/2019/04/profile-card-design"
-          >
-            here
-          </a>
-          - Design made by
-          <a
-            target="_blank"
-            href="https://dribbble.com/shots/6276930-Profile-Card-UI-Design"
-          >
-            Ildiesign
-          </a>
-        </p>
-      </footer>
     </div>
   );
 };
